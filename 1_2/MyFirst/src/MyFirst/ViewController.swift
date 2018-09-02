7991dff7ce0d92b8ab02270bb3c37f00979f7e03
Yoshinori Kobayashi <yoshiiikoba@gmail.com>
2018-09-03 06:50:32 +0900

    1-2

--- 1_2/MyFirst/src/MyFirst/ViewController.swift
+++ 1_2/MyFirst/src/MyFirst/ViewController.swift
@@ -2,29 +2,23 @@
 //  ViewController.swift
 //  MyFirst
 //
-//  Created by Swift-Beginners on 2017/08/19.
-//  Copyright © 2017年 Swift-Beginners. All rights reserved.
+//  Created by Swift-Beginners on 2018/08/31.
+//  Copyright © 2018年 Swift-Beginners. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
 
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
-
-    // (1)  ラベルに文字を入れる
-    outputLabel.text = "Hello Swift!"
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
     
-  }
+        // （１）　ラベルに文字を入れる
+        outputLabel.text = "Hello Swift!"
+    }
 
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
-  }
-
-  @IBOutlet weak var outputLabel: UILabel!
-  
+    @IBOutlet weak var outputLabel: UILabel!
+    
 }
 
